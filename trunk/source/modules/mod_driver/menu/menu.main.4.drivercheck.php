8395f99bb3b19e01122372a4afbf6d47a1fcf99d
inancm.dev@gmail.com <inancm.dev@gmail.com@eec16573-1868-54e4-3c89-b149cd2b3ff4>
2013-09-25 05:46:36 +0000

    add history map customer and driver

--- trunk/source/modules/mod_driver/menu/menu.main.4.drivercheck.php
+++ trunk/source/modules/mod_driver/menu/menu.main.4.drivercheck.php
@@ -1,6 +1,6 @@
 <li style="background:none; border:none;">
 	<a href="index.php?p=driver.drivercheck&menu=main_taxi" 
     style="background-image: url(modules/mod_driver/images/drivercheck-icon.png); padding: 60px 8px 8px;">
-    	<div style="color:#F0F;"><? echo $lang_menu["driver.drivercheck"]?></div>
+    	<div><? echo $lang_menu["driver.drivercheck"]?></div>
     </a>
 </li>
--- trunk/source/modules/mod_user/menu/menu.main.6.customercheck.php
+++ trunk/source/modules/mod_user/menu/menu.main.6.customercheck.php
@@ -1,7 +1,7 @@
 <li style="background:none; border:none;">
 	<a href="index.php?p=user.customercheck&menu=main_user" 
     style="background-image: url(modules/mod_user/images/customercheck-icon.png);padding: 60px 8px 8px;">
-    	<div style="color:#F0F;"><? echo $lang_menu["user.customercheck"] ?></div>
+    	<div><? echo $lang_menu["user.customercheck"] ?></div>
     </a>
 </li>
 
